6bc297463a7734401ceb1482aff5b3e3a7310cbb
MatteoCiardi <ciardimatteo99@gmail.com>
2025-09-08 19:01:55 +0200

    aggiunti commenti

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // GENERATORE DI 5 NUMERI CASUALI (1, 50)
+
+// FUNZIONE PER GENERARE UN NUMERO
 function randomNumGen(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// FUNZIONE PER CONTROLLARE SE IL NUMERO EISTE IN UN ARRAY, SE SI, NE GENERA UN ALTRO
 function generateFiveUniqueNumbers(min, max, count) {
     const numbers = [];
 
@@ -17,11 +20,12 @@ function generateFiveUniqueNumbers(min, max, count) {
     return numbers;
 }
 
-let randomNumArr = generateFiveUniqueNumbers(1, 50, 5);
+let randomNumArr = generateFiveUniqueNumbers(1, 50, 5); //variabile con funzione assegnata
 
 // CREAZONE E ASSEGNAZIONE CONTENUTO UL
 const numList = document.getElementById("numbers-list");
 
+// FUNZIONE PER CREARE UN ELEMENTO LI ED ASSEGNARGLI UNO DEI NUMERI RANDOM
 function randomNumList() {
     for (let i = 0; i < randomNumArr.length; i++) {
         const listItem = document.createElement("li");
@@ -33,13 +37,14 @@ function randomNumList() {
 randomNumList();
 
 // COUNTDOWN
-let seconds = 3;
+let seconds = 30;
 const countdownDisplay = document.getElementById("countdown");
 const answerInput = document.getElementById("answers-form");
 const altText = document.getElementById("instructions");
 
-countdownDisplay.innerText = seconds--;
+countdownDisplay.innerText = seconds--; // mostra i secondi che scendono
 
+// FA PARTIRE IL COUNTDOWN E ALLA FINE ESEGUE I COMANDI CHE SONO NELL'IF
 const intervalId = setInterval(function () {
     if (seconds < 0) {
         countdownDisplay.classList.add("d-none");
@@ -55,20 +60,23 @@ const intervalId = setInterval(function () {
 const userInput = document.querySelectorAll("input");
 const myForm = document.getElementById("answers-form");
 
+// EVENTO CHE FA PARTIRE LA VALIDAZIONE AL MOMENTO DELL'INVIO 
 myForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     let count = 0
     let correctNum = [];
 
+    // CICLO CHE CONTROLLA SE I NUMERI INSERITI CORRISPONDONO A QUELLI GENERATI
     for (let i = 0; i < userInput.length; i++) {
-        const userNum = parseInt(userInput[i].value);
+        const userNum = parseInt(userInput[i].value); //converto l'input in numero e lo salvo in una variabile
         if (randomNumArr.includes(userNum)) {
             count++;
             correctNum.push(userNum);
         }
     }
 
+    // STAMPA MESSAGGIO IN BASE ALL'ESITO DELLA VALIDAZIONE
     const message = document.getElementById("message");
 
     if (count === 0) {
@@ -79,6 +87,8 @@ myForm.addEventListener("submit", (e) => {
         message.textContent = `Hai indovinato ${count} numero/i: ${correctNum.join(", ")}`;
     }
 });
+
+// FINE
 
 
 
